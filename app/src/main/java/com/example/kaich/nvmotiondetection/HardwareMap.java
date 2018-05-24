028617055955b063a42457bc1a97f8678e40e4b8
Kaichen Liu <kobereincarnate@gmail.com>
2018-05-23 21:57:36 -0700

    initial bug fixes, temp layout
    correct wrong compileVersion, made new buttons and camera frame

---
+++ app/src/main/java/com/example/kaich/nvmotiondetection/HardwareMap.java
@@ -0,0 +1,14 @@
+package com.example.kaich.nvmotiondetection;
+
+import android.os.Build;
+
+public class HardwareMap {
+
+    public int cameraXRes;
+    public int cameraYRes;
+    public int cameraFPS;
+    public double aspectRatio;
+
+    public int displayWidth;
+    public int displayHeight;
+}
---
+++ app/src/main/java/com/example/kaich/nvmotiondetection/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.kaich.nvmotiondetection;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.opencv.core.Core;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.picture_mode:
+                    return true;
+                case R.id.video_mode:
+                    return true;
+                case R.id.camera:
+                    return true;
+                case R.id.storage:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.loadLibrary("opencv_java3");
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
