3d0886feaae8f08a89bcffecf774395b2bcc4f61
Kaichen Liu <kobereincarnate@gmail.com>
2018-07-19 08:05:35 -0700

    permissions

--- app/src/main/java/com/example/kaich/nvmotiondetection/MainActivity.java
+++ app/src/main/java/com/example/kaich/nvmotiondetection/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kaich.nvmotiondetection;
 
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
@@ -11,6 +14,9 @@ import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Surface;
 import android.view.TextureView;
@@ -18,6 +24,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 import android.widget.VideoView;
 import android.util.Size;
 
@@ -26,8 +33,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     //HSI
     private CameraDevice mCameraDevice;
+
     //UI
     private Toolbar mTopToolbar;
     private SeekBar mSeekBar;
@@ -83,38 +92,48 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    //analytical
-    private VisionProcess mVisionProcess;
+    //permissions
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 500;
+    private static final int MY_PERMISSIONS_REQUEST_STORAGE = 600;
 
-    //file saving
-    private File mFile;
-    private Handler mBackgroundHandler;
-    private HandlerThread mBackgroundThread;
+    //analytical
+    private VisionProcess VisionProcess;
+
+    //file storage
+    private File File;
+    private Handler BackgroundHandler;
+    private HandlerThread BackgroundThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         System.loadLibrary("opencv_java3"); //do NOT use Core.NATIVE_LIBRARY_NAME, OpenCV is already treated as local in jni
 
         mTopToolbar = findViewById(R.id.topToolbar);
-        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
-        mSeekBar.setOnSeekBarChangeListener((mOnSeekBarChangeListener));
+        mSeekBar = findViewById(R.id.seekBar);
         mSunBrightness = findViewById(R.id.sunBrightness);
-        mSunBrightness.setOnClickListener(mOnSunBrightnessClickListnener);
         mMoonBrightness = findViewById(R.id.moonBrightness);
-        mMoonBrightness.setOnClickListener(mOnMoonBrightnessClickListener);
+
 
         mTextureView = findViewById(R.id.textureView);
-
         mBottomToolbar = findViewById(R.id.bottomToolbar);
         mCameraButton = findViewById(R.id.cameraButton);
-        mCameraButton.setOnClickListener(mOnCameraClickListener);
         mRecordTypeButton = findViewById(R.id.recordTypeButton);
-        mRecordTypeButton.setOnClickListener(mOnRecordTypeClickListener);
         mSwitchCameraButton = findViewById(R.id.switchCameraButton);
-        mSwitchCameraButton.setOnClickListener(mOnSwitchCameraClickListener);
+
+        //listeners not set until permissions granted
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},MY_PERMISSIONS_REQUEST_CAMERA);
+        }else if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_STORAGE);
+        }else{
+            //all permissions already granted
+            openCamera();
+        }
 
     }
 
@@ -144,12 +163,48 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
     }
 
     private void takeVideo(){
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
+        switch(requestCode){
+            case MY_PERMISSIONS_REQUEST_CAMERA: //consecutive permission requests
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_STORAGE);
+                    }else{
+                        //permission already granted
+                    }
+
+                }else{
+                    Toast.makeText(this, "This application cannot function without camera permissions", 3);
+                }
+                return;
+            case MY_PERMISSIONS_REQUEST_STORAGE: //can also feed from if user exited after first permission
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    //all permissions granted, enable functionality
+
+                    mSeekBar.setOnSeekBarChangeListener((mOnSeekBarChangeListener));
+                    mSunBrightness.setOnClickListener(mOnSunBrightnessClickListnener);
+                    mMoonBrightness.setOnClickListener(mOnMoonBrightnessClickListener);
+
+                    mCameraButton.setOnClickListener(mOnCameraClickListener);
+                    mRecordTypeButton.setOnClickListener(mOnRecordTypeClickListener);
+                    mSwitchCameraButton.setOnClickListener(mOnSwitchCameraClickListener);
+
+
+                }else{
+                    Toast.makeText(this, "This application cannot function without storage permissions", 3);
+                }
+                return;
+        }
+
+    }
 }
